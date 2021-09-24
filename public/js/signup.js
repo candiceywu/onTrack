0800e31dd59461281028ff2deab8786c4764c3a7
Scotty Yackzan <syackzan@gmail.com>
2021-09-24 15:14:46 -0700

    Updated signup.js to pass input values into POST request

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,27 +1,20 @@
 //Passing in Values Logged in Submit Form//
-const oUsername = document.getElementById('oUsername').value.trim();
-const oEmail = document.getElementById('oEmail').value.trim();;
-const oFirstname = document.getElementById('oFirstname').value.trim();
-const oLastname = document.getElementById('oLastname').value.trim();
-const oNumber = document.getElementById('oNumber').value.trim();
-const oPassword = document.getElementById('oPassword').value.trim();
-const oPasswordcheck = document.getElementById('oPasswordcheck').value.trim();
-const submitOBtn = document.getElementById('submitBtn').value.trim();
-
-const gcUsername = document.getElementById('gcUsername').value.trim();
-const gcEmail = document.getElementById('gcEmail').value.trim();;
-const gcCompanyName = document.getElementById('gcCompanyName').value.trim();
-const gcLicense = document.getElementById('gcLicense').value.trim();
-const gcNumber = document.getElementById('gcNumber').value.trim();
-const gcPassword = document.getElementById('gcPassword').value.trim();
-const gcPasswordcheck = document.getElementById('gcPasswordcheck').value.trim();
-const submitGCBtn = document.getElementById('submitGCBtn').value.trim();
+const submitOBtn = document.getElementById('submitBtn')
+const submitGCBtn = document.getElementById('submitGCBtn')
 
 
 
-//Submit Form for Owner//
+//Submit Form for Owner
 const submitOForm = async (event) => {
     event.preventDefault();
+
+    let oUsername = document.getElementById('oUsername').value.trim();
+    let oEmail = document.getElementById('oEmail').value.trim();;
+    let oFirstname = document.getElementById('oFirstname').value.trim();
+    let oLastname = document.getElementById('oLastname').value.trim();
+    let oNumber = document.getElementById('oNumber').value.trim();
+    let oPassword = document.getElementById('oPassword').value.trim();
+    let oPasswordcheck = document.getElementById('oPasswordcheck').value.trim();
 
     if(oPassword !== oPasswordcheck){
         alert("Passwords did not match");
@@ -29,22 +22,30 @@ const submitOForm = async (event) => {
     }
 
     if (oUsername && oEmail && oFirstname && oLastname && oNumber && oPassword) {
-        const response = await fetch('/api/owners', {
+        const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({oUsername, oEmail, oFirstname, oLastname, oNumber, oPassword }),
             headers: { 'Content-Type': 'application/json'}
         });
     };
 
-    if (response.ok) {
-        document.location.replace('/projects');
-    } else {
-        alert('Failed to Sign Up');
-    }
+    // if (response.ok) {
+    //     document.location.replace('/projects');
+    // } else {
+    //     alert('Failed to Sign Up');
+    // }
 }
 
 const submitGCForm = async (event) => {
     event.preventDefault();
+
+    let gcUsername = document.getElementById('gcUsername').value.trim();
+    let gcEmail = document.getElementById('gcEmail').value.trim();;
+    let gcCompanyName = document.getElementById('gcCompanyName').value.trim();
+    let gcLicense = document.getElementById('gcLicense').value.trim();
+    let gcNumber = document.getElementById('gcNumber').value.trim();
+    let gcPassword = document.getElementById('gcPassword').value.trim();
+    let gcPasswordcheck = document.getElementById('gcPasswordcheck').value.trim();
 
     if(gcPassword !== gcPasswordcheck){
         alert("Passwords did not match");
@@ -52,18 +53,18 @@ const submitGCForm = async (event) => {
     }
 
     if (gcUsername && gcEmail && gcCompanyName && gcLicense && gcNumber && gcPassword){
-        const response = await fetch('/api/gc', {
+        const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({gcUsername, gcEmail, gcCompanyName, gcLicense, gcNumber, gcPassword }),
             headers: { 'Content-Type': 'application/json'}
         });
     };
 
-    if (response.ok) {
-        document.location.replace('/projects');
-    } else {
-        alert('Failed to Sign Up');
-    }
+    // if (response.ok) {
+    //     document.location.replace('/');
+    // } else {
+    //     alert('Failed to Sign Up');
+    // }
 }
 
 submitOBtn.addEventListener('click', submitOForm);
