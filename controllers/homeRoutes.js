40132a8d3a873176e98f1e0fa2ca26e39a8cd6c3
Scotty Yackzan <syackzan@gmail.com>
2021-09-24 09:14:28 -0700

    Added homepage route for signup form html + added signup handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,6 +26,15 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/signup', (req, res) => {
+  // if (req.session.logged_in) {
+  //   res.redirect('/');
+  //   return;
+  // }
+
+  res.render('signup');
+});
+
 
 
 // GET one project
