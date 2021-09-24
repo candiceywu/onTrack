ff31fa981eee5377f09a6b75f3134bd269dea995
Scotty Yackzan <syackzan@gmail.com>
2021-09-24 08:44:46 -0700

    Updated login handlebar page with more html

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,10 +18,10 @@ router.get('/', async (req, res) => {
 
 
 router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
+  // if (req.session.logged_in) {
+  //   res.redirect('/');
+  //   return;
+  // }
 
   res.render('login');
 });
@@ -76,14 +76,6 @@ router.get('/scope/:id', async (req, res) => {
       res.status(500).json(err);
     }
   }
-});
-
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-  res.render('login');
 });
 
 module.exports = router;
--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-const ownerBtn = document.getElementById('ownerbtnjs');
-const gcBtn = document.getElementById('gcbtnjs');
-
-function printHello(){
-    console.log("hello");
-}
-
-ownerBtn.addEventListener('click', printHello);
-gcBtn.addEventListener('click', printHello);
