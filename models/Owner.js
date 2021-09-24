5c8224cc9b269156db582e32e38a70c3d3346949
Scotty Yackzan <syackzan@gmail.com>
2021-09-24 12:47:53 -0700

    added Last_name and switched around model data in Owner File

--- models/Owner.js
+++ models/Owner.js
@@ -12,27 +12,26 @@ Owner.init(
             primaryKey: true,
             auto_increment: true,
         },
-
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-
         username: {
             type: DataTypes.STRING,
             allowNull: false,
         },
-
         email: {
             type: DataTypes.STRING,
             allowNull: false,
         },
-
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
         phone_number: {
             type: DataTypes.STRING,
             allowNull: false,
         },
-
         password: {
             type: DataTypes.STRING,
             allowNull: false,
