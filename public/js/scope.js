49712e4597d71c5ab7113c8a397bfd4b22286279
candiceywu <candice.ying.wu@gmail.com>
2021-09-27 10:58:35 -0700

    initial scope.handlebars and scope.js files

---
+++ public/js/scope.js
@@ -0,0 +1,49 @@
+const myModal = $('#myModal');
+const newScope = $('#newScope');
+const addScopeBtn = document.getElementById('addScopeBtn');
+
+
+
+function openScopeModal (){
+    $(function () {
+        myModal.modal("show");
+    });
+}
+
+const addNewScope = async (event) => {
+    event.preventDefault();
+
+    // do we need to add picture
+    
+    let converter = false;
+    let title = document.getElementById('title').value.trim();
+    let description = document.getElementById('description').value.trim();
+    let is_complete = parseInt(document.getElementById('is_complete').value.trim());
+    let picture = document.getElementById('picture').value.trim();
+
+    console.log(title);
+    console.log(description);
+    console.log(is_complete);
+
+    if (name && address && cost && description && owner){
+        const response = await fetch ('/api/scope', {
+            method: 'POST',
+            body: JSON.stringify({title, description, is_complete, picture }),
+            headers: {'Content-Type': 'application/json'}
+        });
+        
+        converter = true;
+    }
+
+    if (converter){
+        $(function () {
+            myModal.modal("hide");
+            location.reload();
+        });
+    }
+}
+
+
+
+newScope.click(openScopeModal);
+addScopeBtn.addEventListener('click', addNewScope);
