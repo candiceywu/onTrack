969411587f3cf256c4f023f8e9f1bec97705ec20
candiceywu <candice.ying.wu@gmail.com>
2021-09-27 14:19:06 -0700

    scopes modal matches projects modal

--- controllers/api/scopeRoutes.js
+++ controllers/api/scopeRoutes.js
@@ -2,48 +2,78 @@ const router = require('express').Router();
 const { Scope } = require('../../models')
 
 
+//POST route for new scope
+router.post('/', async (req, res) => {
+  try {
+    console.log(req.body);
+    const newScope = await Scope.create({
+      title: req.body.title,
+      description: req.body.description,
+      is_complete: req.body.is_complete,
+    });
 
+    res.status(200).json(newScope);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// //GET all scopes on dashboard
+// router.get('/', async (req, res) => {
+//   try {
+//     // Get all scopes, sorted by name
+//     const scopeData = await User.findAll({
+
+//       // Serialize user data so templates can read it
+//       const scopes = scopeData.map((scope) => scope.get({ plain: true }));
+//     });
+//     // Pass serialized data into Handlebars.js template
+//     res.render('scope', { scopes });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // GET one scope
 //need to create an option for gc to edit a scope
 router.get('/scope/:id', async (req, res) => {
-    // If the user is not logged in, redirect the user to the login page
-    if (!req.session.loggedIn) {
-      res.redirect('/login');
-    } else {
-      // If the user is logged in, allow user to view the scope
-      try {
-        const scopeData = await Scope.findByPk(req.params.id);
-        const scope = scopeData.get({ plain: true });
-        res.render('scope', { 
-          scope, 
-          isContractor: req.session.isContractor 
-        });
-      } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-      }
-    }
-  });
-  
-
-
-  // PUT scopes
-  router.put('/scope/:id', async (req, res) => {
+  // If the user is not logged in, redirect the user to the login page
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+  } else {
+    // If the user is logged in, allow user to view the scope
     try {
-      const gcData = await Scope.update(req.body, {
-        where: {
-          id: req.params.id
-        },
-        individualHooks: true
+      const scopeData = await Scope.findByPk(req.params.id);
+      const scope = scopeData.get({ plain: true });
+      res.render('scope', {
+        scope,
+        isContractor: req.session.isContractor
       });
-      if (!gcData[0]) {
-        res.status(404).json({ message: 'Sorry, you can\'t modify this data.' });
-        return;
-      } res.status(200).json(gcData);
     } catch (err) {
-      res.status(500).json(err)
-    };
-  });
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+});
 
-  module.exports = router;
+
+
+// PUT scopes
+router.put('/scope/:id', async (req, res) => {
+  try {
+    const gcData = await Scope.update(req.body, {
+      where: {
+        id: req.params.id
+      },
+      individualHooks: true
+    });
+    if (!gcData[0]) {
+      res.status(404).json({ message: 'Sorry, you can\'t modify this data.' });
+      return;
+    } res.status(200).json(gcData);
+  } catch (err) {
+    res.status(500).json(err)
+  };
+});
+
+module.exports = router;
