b6e0ec5b2dcce7d2e9c0b5153d420fa67deec076
Scotty Yackzan <syackzan@gmail.com>
2021-09-27 17:24:29 -0700

    Added a scopeId scripts to updat PUT request for scope

---
+++ public/js/scopeId.js
@@ -0,0 +1,22 @@
+let storedScopeId = document.getElementById('storedScopeId');
+const editScopeBtn = document.getElementById('editScopeBtn');
+
+const editScope = async () => {
+
+    let editTitle = document.getElementById('edit-title');
+    let editDescription = document.getElementById('edit-description');
+    let status = document.getElementById('edit-is_complete');
+
+    const scopeChange = await fetch (`/api/scope/:${storedScopeId}`, { 
+        method: "PUT",
+        body: JSON.stringify({ editTitle, editDescription, status}),
+        headers: {'Content-Type': 'application/json'}
+    })
+
+    window.location.assign('/scope')
+
+}
+
+editScopeBtn.addEventListener('click', editScope);
+
+<script src="/js/scopeId.js"></script>
