87199def043bd6a715338b4549092443b5341e16
Scotty Yackzan <syackzan@gmail.com>
2021-09-27 17:32:28 -0700

    added Trims

--- public/js/scopeId.js
+++ public/js/scopeId.js
@@ -3,9 +3,9 @@ const editScopeBtn = document.getElementById('editScopeBtn');
 
 const editScope = async () => {
 
-    let editTitle = document.getElementById('edit-title');
-    let editDescription = document.getElementById('edit-description');
-    let status = document.getElementById('edit-is_complete');
+    let editTitle = document.getElementById('edit-title').value.trim();
+    let editDescription = document.getElementById('edit-description').value.trim();
+    let status = document.getElementById('edit-is_complete').value.trim();
 
     const scopeChange = await fetch (`/api/scope/:${storedScopeId}`, { 
         method: "PUT",
